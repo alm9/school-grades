12d5af12f53e2046a5e9d4bc7bf8fafc181fe77d
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-24 16:52:35 -0300

    test to see loading of grades data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import * as api from './api/apiService';
 
 // export default function App() {
 function App() {
-  const testApi = async () => {
-    // console.log(await api.getAllGrades());
-    const res = await api.getAllGrades();
-    console.log(res);
-  };
-  testApi();
+  // teste da api removido
+  //usando hooks:
+  const [allGrades, setAllGrades] = useState([]);
+  const [selectedGrade, setSelectedGrade] = useState({});
+  const [isModelOpen, setIsModelOpen] = useState(false);
+
+  // useEffect(() => {
+  //   api.getAllGrades().then((grades) => setAllGrades(grades));
+  // }, []);
+  //FORÇANDO ATRASO PARA VER CARREGANDO: (usando async await)
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = await api.getAllGrades();
+      setTimeout(() => {
+        setAllGrades(grades);
+      }, 2000);
+    };
+    getGrades();
+  }, []);
+  //faz a mesma coisa que acima, mas usando fetch
+  // useEffect(() => {
+  //   api.getAllGrades().then((grades) => {
+  //     setTimeout(() => {
+  //       setAllGrades(grades);
+  //     }, 1000);
+  //   });
+  // }, []);
 
   return (
     <div className="App">
-      <header className="App-header">Eae Dé</header>
+      <header className="App-header">
+        <h1 className="container">Controle de notas</h1>
+        {allGrades.length > 0 && <p>Notas disponíveis</p>}
+        {allGrades.length == 0 && <p>Carregando notas...</p>}
+      </header>
     </div>
   );
 }
