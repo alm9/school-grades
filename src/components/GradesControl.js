72d9a1dc94e8f4373866e4d027ab051a8c8cafc3
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-25 00:02:56 -0300

    showing all info together in one table

--- src/components/GradesControl.js
+++ src/components/GradesControl.js
@@ -1,5 +1,37 @@
 import React from 'react';
 
 export default function GradesControl({ grades, onDelete, onPersist }) {
-  return <div>{JSON.stringify(grades)}</div>;
+  //TODO: array tableGrades compondo diversos arrays agrupados por nome e disciplina,
+  //para não deixar tudo jogado em uma tabela só, como está no momento
+
+  //O materialize fornece a classe 'striped', que distingue uma linha da outra:
+  return (
+    <div className="container">
+      <table className="striped">
+        <thead>
+          <th>Aluno</th>
+          <th>Disciplina</th>
+          <th>Avaliação</th>
+          <th>Nota</th>
+          {/*&nbsp; = char especial do html para algo ficar vazio (non break space) */}
+          <th>&nbsp;</th>
+        </thead>
+        <tbody>
+          {grades.map(({ id, subject, student, type, value, isDeleted }) => {
+            return (
+              <tr key={id}>
+                <td>{student}</td>
+                <td>{subject}</td>
+                <td>{type}</td>
+                <td>{value}</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
 }
