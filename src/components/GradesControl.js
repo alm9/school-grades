968aa94c6c432a15005073f87db15e9e1661745e
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-25 12:46:42 -0300

    tableGrades working, but need css

--- src/components/GradesControl.js
+++ src/components/GradesControl.js
@@ -1,37 +1,75 @@
 import React from 'react';
 
 export default function GradesControl({ grades, onDelete, onPersist }) {
-  //TODO: array tableGrades compondo diversos arrays agrupados por nome e disciplina,
-  //para não deixar tudo jogado em uma tabela só, como está no momento
+  //Array tableGrades compõe diversos arrays agrupados por nome e disciplina
+  const tableGrades = [];
+
+  let currentStudent = grades[0].student,
+    currentSubject = grades[0].subject,
+    currentGrades = [],
+    id = 1;
+
+  grades.forEach((grade) => {
+    if (grade.subject !== currentSubject) {
+      tableGrades.push({
+        id: id++,
+        student: currentStudent,
+        subject: currentSubject,
+        grades: currentGrades,
+      });
+      currentSubject = grade.subject;
+      currentGrades = [];
+    }
+
+    if (grade.student !== currentStudent) currentStudent = grade.student;
+
+    currentGrades.push(grade);
+  });
+  //Após o loop, inserir o último elemento
+  tableGrades.push({
+    id: id++,
+    student: currentStudent,
+    subject: currentSubject,
+    grades: currentGrades,
+  });
+  console.log(tableGrades);
 
   //O materialize fornece a classe 'striped', que distingue uma linha da outra:
   return (
     <div className="container">
-      <table className="striped">
-        <thead>
-          <th>Aluno</th>
-          <th>Disciplina</th>
-          <th>Avaliação</th>
-          <th>Nota</th>
-          {/*&nbsp; = char especial do html para algo ficar vazio (non break space) */}
-          <th>&nbsp;</th>
-        </thead>
-        <tbody>
-          {grades.map(({ id, subject, student, type, value, isDeleted }) => {
-            return (
-              <tr key={id}>
-                <td>{student}</td>
-                <td>{subject}</td>
-                <td>{type}</td>
-                <td>{value}</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+      {tableGrades.map(({ id, grades }) => {
+        return (
+          <table className="striped" key={id}>
+            <thead>
+              <tr>
+                <th>Aluno</th>
+                <th>Disciplina</th>
+                <th>Avaliação</th>
+                <th>Nota</th>
+                {/*&nbsp; = char especial do html para algo ficar vazio (non break space) */}
+                <th>&nbsp;</th>
               </tr>
-            );
-          })}
-        </tbody>
-        <tfoot></tfoot>
-      </table>
+            </thead>
+            <tbody>
+              {grades.map(
+                ({ id, subject, student, type, value, isDeleted }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{student}</td>
+                      <td>{subject}</td>
+                      <td>{type}</td>
+                      <td>{value}</td>
+                      <td>&nbsp;</td>
+                      <td>&nbsp;</td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+            <tfoot></tfoot>
+          </table>
+        );
+      })}
     </div>
   );
 }
